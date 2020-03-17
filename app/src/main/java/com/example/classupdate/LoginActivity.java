755628a441aaeca1d83b38cc80e2b3f6897595e6
Dristy03 <anannadristy03@gmail.com>
2020-03-17 17:30:47 +0600

    collapsing toolbar with scrollview added.

--- app/src/main/java/com/example/classupdate/LoginActivity.java
+++ app/src/main/java/com/example/classupdate/LoginActivity.java
@@ -1,23 +1,23 @@
 package com.example.classupdate;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
-
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -137,6 +137,7 @@ public class LoginActivity extends AppCompatActivity {
                 });
     }
     private void loginUser(String email, String password) {
+        Log.d(TAG, "loginUser: started login "+email +" "+ password);
 
         mAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
--- app/src/main/java/com/example/classupdate/MainActivity.java
+++ app/src/main/java/com/example/classupdate/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.classupdate;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,17 +9,24 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
-    private Button btnLogout;
+    private FloatingActionButton btnLogout;
+    private CardView cardClassRoutine,cardExamRoutine,cardNotification,cardResources,cardCR,cardProfile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnLogout= findViewById(R.id.btnLogout);
-
+        cardClassRoutine=findViewById(R.id.cardClassRoutine);
+        cardCR=findViewById(R.id.cardCR);
+        cardExamRoutine=findViewById(R.id.cardExamRoutine);
+        cardNotification=findViewById(R.id.cardNotification);
+        cardResources=findViewById(R.id.cardResources);
+        cardProfile=findViewById(R.id.cardProfile);
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -27,6 +35,48 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
+        cardClassRoutine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        cardExamRoutine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });cardClassRoutine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        cardResources.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        cardNotification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        cardCR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        cardProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
 
     }
 }
