496a3c4c9293bc4d43a4ce39b6cb65471d12498e
Dristy03 <anannadristy03@gmail.com>
2020-03-25 20:05:39 +0600

    Log_out button added

--- app/src/main/java/com/example/classupdate/MainActivity.java
+++ app/src/main/java/com/example/classupdate/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
